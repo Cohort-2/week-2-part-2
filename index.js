375f70afd7d7e064c127810b2d4e6447bafde2e8
Subhajit Chaudhury <subhajitchaudhury05@gmail.com>
2024-09-14 16:30:14 +0530

    learned query params

--- index.js
+++ index.js
@@ -2,8 +2,18 @@ const express = require("express");
 
 const app = express();
 
+function sum(n) {
+  let ans = 0;
+  for (let i = 1; i <= n; i++) {
+    ans += i;
+  }
+  return ans;
+}
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  const n = req.query.n;
+  const ans = sum(n);
+  res.send("Your answer is " + ans);
 });
 
 app.listen(3000, () => {
